a5bc9ad42db36f43366bfa429466afdce6cb5682
faketogosamogo <42639694+faketogosamogo@users.noreply.github.com>
2020-02-15 01:19:45 +0700

    правки постгрес

--- models/db.go
+++ models/db.go
@@ -2,12 +2,13 @@ package models
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	)
+	"os"
+	_ "github.com/lib/pq"
+)
 
-const (
-	DriverName = "mysql"
-	ConnectionString = "root:1234@/guess_number"
+var (
+	DriverName = "postgres"
+	ConnectionString = os.Getenv("DATABASE_URL")
 )
 
 type DB struct {
