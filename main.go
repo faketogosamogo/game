c10f1ee11948c02b2ab88bc20fa650d1761e3e79
faketogosamogo <42639694+faketogosamogo@users.noreply.github.com>
2020-02-15 03:07:27 +0700

    mysql

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"GuessNumber/controllers"
+	"GuessNumber/game"
+	"GuessNumber/models"
 	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
-
-	//"os"
 	"time"
 )
 
 func main(){
 	rand.Seed(time.Now().UTC().UnixNano())
 	port := os.Getenv("PORT")
-	/*log.Println(port)
+	log.Println(port)
 
 	gameManager, err := game.NewGameManager()
 	if err!=nil{
@@ -25,7 +26,7 @@ func main(){
 		log.Println("Ошибка подключения к БД")
 	}
 	controllers.GameManager = gameManager
-*/
+
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.HomeController)
